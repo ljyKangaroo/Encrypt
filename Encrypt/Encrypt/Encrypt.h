0f9c5995a13a594ffe6996a9126b5de3adb23e59
Karo <623039489@qq.com>
2022-04-11 18:00:07 +0800

    feat(加密)：项目提交

---
+++ Encrypt/Encrypt/Encrypt.h
@@ -0,0 +1,18 @@
+//
+//  Encrypt.h
+//  Encrypt
+//
+//  Created by admin on 2022/4/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Encrypt.
+FOUNDATION_EXPORT double EncryptVersionNumber;
+
+//! Project version string for Encrypt.
+FOUNDATION_EXPORT const unsigned char EncryptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Encrypt/PublicHeader.h>
+
+
